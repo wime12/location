9d2e1a43c5ea040a8ac952e0dd7c0f828cf00af7
Wilfried Meindl <wilfried.meindl@gmail.com>
2015-10-05 13:49:51 +0200

    -h option. Additions to man page.
    * discard the -e option
    * introduce -h option
    * complete man page

--- location.sh
+++ location.sh
@@ -5,19 +5,21 @@ SHLIB=~/.shlib
 . $SHLIB/urlencode.sh
 
 separator="|"
-script=0
+script=1
 
-while getopts es: opt; do
+while getopts hs: opt; do
   case $opt in
-    e) script=1
-       shift
+    h) script=0
       ;;
     s) separator=$OPTARG
-       shift 2
   esac
 done
 
+shift $((OPTIND-1))
+
 separator="${separator:-|}"
 
-fetch -q -o - "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text=%27$(urlencode "$1")%27" |
-awk  -v script=$script -v separator="$separator" -f $AWKLIB/getxml.awk -f $AWKLIB/location.awk /dev/stdin
+#fetch -q -o - \
+curl -m 4 -s \
+"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text=%27"$(urlencode "$1")"%27" |
+awk -v script=$script -v separator="$separator" -f $AWKLIB/getxml.awk -f $AWKLIB/location.awk /dev/stdin
