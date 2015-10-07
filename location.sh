c55927ff50e03170a125ef3482944a251b2c3bf7
Wilfried Meindl <wilfried.meindl@gmail.com>
2015-10-07 14:47:50 +0200

    Use AWKFILE_DIR in awk call

--- location.sh
+++ location.sh
@@ -24,4 +24,4 @@ separator="${separator:-|}"
 #curl -m 4 -s \
 fetch -q -o - \
 "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text=%27"$(urlencode "$1")"%27" |
-awk -v script=$script -v separator="$separator" -f $AWKLIB/getxml.awk -f $PREFIX/share/location/location.awk /dev/stdin
+awk -v script=$script -v separator="$separator" -f $AWKLIB/getxml.awk -f $AWKFILE_DIR/location.awk /dev/stdin
